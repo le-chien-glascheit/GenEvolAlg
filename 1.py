33b0b8687ec29371005144fd51d84a5c162ea5b1
le_shien <4_lapa@mail.ru>
2024-11-19 17:47:11 +0300

    добавлено пространство поиска метод выведен в отдельную функцию как на схеме

--- 1.py
+++ 1.py
@@ -1,25 +1,31 @@
 import random
 
-i = 0
-L = 15
-N = L
-max = 0
-maxS = ''
-si = ''
+def monte_carlo(maxS,L,N):
+    i = 0
+    max = 0
+    while i < N:
+            si = ''.join(random.choice(L))
+            print(f'Шаг {i+1}: si = {si}')
+            if int(si, 2) > max:
+                max = int(si, 2)
+                maxS = si
+                print("\033[031m" + f'Новый максимум приспособленности = {max}' + "\033[0m")
 
-while i < N:
-        si = ''.join(random.choice('01') for _ in range(L))
-        print(f'Шаг {i+1}: si = {si}')
-        if int(si, 2) > max:
-            max = int(si, 2)
-            maxS = si
-            print(f"\033[031m" + 'Новый максимум приспособленности = {max}' + "\033[0m")
+            print(f'\tТекущий максимум приспособленности = {max}')
+            print(f'\tКодировка = {maxS}')
+            print(f'\tВыбираемая кодировка на текущем шаге: = {si}')
+            print(f"\tПриспособленность выбираемой кодировки = {int(si, 2)} ")
+            print('\n\r')
 
-        print(f'\tТекущий максимум приспособленности = {max}')
-        print(f'\tКодировка = {maxS}')
-        print(f'\tВыбираемая кодировка на текущем шаге: = {si}')
-        print(f"\tПриспособленность выбираемой кодировки = {int(si, 2)} ")
-        print('\n\r')
+            i += 1
+    print(f'Приспособленность = {max}, Кодировка = {maxS}')
 
-        i += 1
-print(f'Приспособленность = {max}, Кодировка = {maxS}')
+if __name__ == '__main__':
+    i = 0
+    L = 15 # ландшафт поиска
+    N = 32 # кол-во кодировок
+    max = 0 # макс приспособленность
+    maxS = '' # кодировка и решение
+    si = '' # кодировка
+    xi = [format(x, 'b').zfill(L) for x in range(2 ** L)]
+    monte_carlo(maxS=maxS,L=xi,N = 32)
