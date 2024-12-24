955dd7cdb246f3a919895ada438b0d84a41429e8
le_shien <4_lapa@mail.ru>
2024-12-24 18:02:52 +0300

    10-я лаба

--- ЛР-10/10.py
+++ ЛР-10/10.py
@@ -3,7 +3,6 @@ import random
 N = 15
 max_weight = 80
 
-# Данные по предметам: (номер, цена, вес)
 items = [
     (1, 21, 2),
     (2, 19, 26),
@@ -23,14 +22,11 @@ items = [
 ]
 
 
-
 def unitInitialization():
-    '''Генерирует особь'''
     return [random.randint(0, 1) for _ in range(N)]
 
 
 def calcFitness(solution):
-    """приспособленность"""
     total_value = sum(items[i][1] * solution[i] for i in range(N))
     total_weight = sum(items[i][2] * solution[i] for i in range(N))
 
@@ -40,15 +36,13 @@ def calcFitness(solution):
 
 
 def crossover(parent1, parent2):
-    """Функция скрещивания"""
     point = random.randint(1, N - 1)
     child = parent1[:point] + parent2[point:]
     return child
 
 
 def mutate(child):
-    """Функция мутации"""
-    mutation_rate = 0.1  # шанс мутации
+    mutation_rate = 0.1
     for i in range(N):
         if random.random() < mutation_rate:
             child[i] = 1 - child[i]
@@ -59,30 +53,26 @@ def main():
     populations_number = int(input("Введите размер популяции: "))
     generations = int(input("Введите количество поколений: "))
 
-    # Инициализация популяции
     population = [unitInitialization() for _ in range(populations_number)]
 
     for generation in range(generations):
         print(f"\nПоколение № {generation + 1}:")
 
-        fitness = [calcFitness(ind) for ind in population]
+        mu = [calcFitness(ind) for ind in population]
 
         for i in range(populations_number):
-            print(f"Индивид {i + 1}: {population[i]}, Приспособленность: {fitness[i]}")
+            print(f"Индивид {i + 1}: {population[i]}, Приспособленность = {mu[i]}")
 
-        # Находим лучшую особь
-        best_index = fitness.index(max(fitness))
-        print("Лучшая особь:", population[best_index], f"| Приспособленность: {fitness[best_index]}")
+        best_index = mu.index(max(mu))
+        print("Лучшая особь:", population[best_index], f" | Приспособленность = {mu[best_index]}")
 
         new_population = []
 
         while len(new_population) < populations_number:
-            # Селекция
             parents_indices = random.sample(range(populations_number), k=2)
             parent1 = population[parents_indices[0]]
             parent2 = population[parents_indices[1]]
 
-            # Скрещивание
             child = crossover(parent1, parent2)
             child = mutate(child)
 
